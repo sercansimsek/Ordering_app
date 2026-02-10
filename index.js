647eb4dea85c81fa7aafdc1bf5864507e6b2091b
Sercan Şimşek <sercansimsek44@gmail.com>
2026-02-10 16:14:12 +0100

    card order created

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import menuArray from "/data.js";
 
 const mainContainer = document.querySelector(".main-container");
+const mainOrder = document.querySelector(".main-order");
 
 function render() {
 	let contentHtml = "";
@@ -25,3 +26,21 @@ function render() {
 }
 
 render();
+
+/* function renderOrderSummary() {
+	let summaryHtml = "";
+
+	summaryHtml = `<section class="main-order">
+					<p class="order-title">Your Order</p>
+					<div class="order-list">
+						<p class="order-name">Pizza</p>
+						<button class="order-btn">remove</button>
+						<p class="order-price">$14</p>
+					</div>
+                    <hr>
+				</section>`;
+
+	mainOrder.innerHTML = summaryHtml;
+}
+
+renderOrderSummary(); */
