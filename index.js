6b1ac0bcafcd52d427b3a9ea72e48119ee3c4a80
Sercan Şimşek <sercansimsek44@gmail.com>
2026-02-13 14:55:57 +0100

    addind items to the card functionality implementation (%35)

--- index.js
+++ index.js
@@ -2,14 +2,23 @@ import menuArray from "/data.js";
 
 const mainContainer = document.querySelector(".main-container");
 const mainOrder = document.querySelector(".main-order");
+const addBtn = document.querySelector(".card-add-btn");
 
-function render() {
-	let contentHtml = "";
+window.addEventListener("click", function (e) {
+	if (e.target.dataset.id) {
+		handleAddItem(e.target.dataset.id);
+	}
+});
+
+function handleAddItem(itemId) {}
+
+function getListOfFood() {
+	let listHtml = "";
 
 	menuArray
 		.map((item) => {
-			const { name, ingredients, price, emoji } = item;
-			contentHtml += `
+			const { name, ingredients, price, emoji, id } = item;
+			listHtml += `
       <div class="main-card">
         <span class="card-icon">${emoji}</span>
         <div class="card-details">
@@ -17,32 +26,36 @@ function render() {
           <p class="details-ingredients">${ingredients.join(", ")}</p>
           <p class="details-price">$${price}</p>
         </div>
-        <button class="card-add-btn">+</button>
+        <button class="card-add-btn" data-id="${id}">+</button>
       </div>
       <hr>
   `;
 		})
 		.join("");
 
-	mainContainer.innerHTML = contentHtml;
+	mainContainer.innerHTML = listHtml;
 }
 
-render();
+function getOrderTotal() {
+	let orderTotalHtml = "";
 
-/* function renderOrderSummary() {
-	let summaryHtml = "";
+	orderTotalHtml = `
+		<p class="order-title">Your Order</p>
+				<div class="order-list">
+					<p class="order-name">Pizza</p>
+					<button class="order-btn">remove</button>
+					<p class="order-price">$14</p>
+				</div>
+				<hr class="order-divider"/>
+				<div class="order-total">
+					<p class="total-title">Total price:</p>
+					<p class="total-price">$14</p>
+				</div>
+				<button class="complete-btn">Complete Order</button>
+	`;
 
-	summaryHtml = `<section class="main-order">
-					<p class="order-title">Your Order</p>
-					<div class="order-list">
-						<p class="order-name">Pizza</p>
-						<button class="order-btn">remove</button>
-						<p class="order-price">$14</p>
-					</div>
-                    <hr>
-				</section>`;
-
-	mainOrder.innerHTML = summaryHtml;
+	mainOrder.innerHTML = orderTotalHtml;
 }
 
-renderOrderSummary(); */
+getListOfFood();
+getOrderTotal();
